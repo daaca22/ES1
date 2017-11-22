c732f45e858a7abcd35adfb50cb81475808c69dd
DanielCoimbraa <daaca@iscte-iul.pt>
2017-11-22 14:51:41 +0000

    Desenho da interface gráfica da geração manual do filtro

--- src/antiSpamFilter/GUI.java
+++ src/antiSpamFilter/GUI.java
@@ -2,27 +2,28 @@ package antiSpamFilter;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
-public class GUI {
+public class GUI extends JComponent {
 
-	private JFrame frame = new JFrame("ISCTE Search");
+	private JFrame frame = new JFrame("Filtro Anti-Spam");
 	private JList list;
 	private Dimension dimension;
 	private String ham;
@@ -44,6 +45,10 @@ public class GUI {
 	}
 
 	public void addFrameContent() {
+		ArrayList<String> listTeste = new ArrayList<String>();
+		Collections.addAll(listTeste, "a1", "str", "mystr", "a1", "str", "mystr", "a1", "str", "mystr", "a1", "str",
+				"mystr", "a1", "str", "mystr", "a1", "str", "mystr", "a1", "str", "mystr", "a1", "str", "mystr", "a1",
+				"str", "mystr");
 
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
@@ -55,6 +60,9 @@ public class GUI {
 		topPanel.setLayout(new BorderLayout());
 		midPanel.setLayout(new BorderLayout());
 		botPanel.setLayout(new BorderLayout());
+		// topPanel.setPreferredSize( new Dimension( 300, 200 ) );
+		midPanel.setPreferredSize(new Dimension(300, 200));
+		botPanel.setPreferredSize(new Dimension(300, 200));
 
 		// aqui são criados os botoes para procurar o caminho dos ficheiros
 		JButton search1 = new JButton("Search");
@@ -63,7 +71,7 @@ public class GUI {
 		// aqui sao criados as textfields para procurar o caminho dos ficheiros
 		JTextField titleText1 = new JTextField("rules.cf");
 		JTextField titleText2 = new JTextField("Ham.log.txt");
-		JTextField titleText3 = new JTextField("Samp.log.txt");
+		JTextField titleText3 = new JTextField("Spam.log.txt");
 
 		// aqui são criados os subpaineis da parte dos caminhos, cada textfield e botao
 		// é um painel
@@ -81,6 +89,7 @@ public class GUI {
 		subpanel3.add(done, BorderLayout.SOUTH);
 		done.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// verificação se os ficheiros estão escolhidos todos e bem.
 				// Aqui será onde os caminhos serão dados para outro metodo para serem abertos e
 				// lidos.
 				// something like: readFiles(give a path);// use this method as much as i want,
@@ -106,10 +115,35 @@ public class GUI {
 			}
 		});
 
-		JScrollPane listScroller = new JScrollPane(list);
-		listScroller.setPreferredSize(new Dimension(40, 40));
+		JButton saveConfig = new JButton("Save");
+		saveConfig.setPreferredSize(new Dimension(70, 50));
+		saveConfig.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// escrever no ficheiro rules.cf
+			}
+		});
+		JButton avaliateConfig = new JButton("Avaliate");
+		avaliateConfig.setPreferredSize(new Dimension(70, 50));
+		avaliateConfig.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// chamar a classe para avaliar!
+			}
+		});
 
-		midPanel.add(listScroller);
+		JPanel results = new JPanel();
+		results = setNumberOfFakes(2, 3);// devolver os valores de FP e FN
+
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new BorderLayout());
+		buttons.add(saveConfig, BorderLayout.NORTH);
+		buttons.add(avaliateConfig, BorderLayout.CENTER);
+		buttons.add(results, BorderLayout.SOUTH);
+
+		JPanel listBoxRules = createListBox(listTeste);
+		JPanel listBoxPesos = createListBox(listTeste);
+		midPanel.add(listBoxRules, BorderLayout.WEST);
+		listBoxRules.add(listBoxPesos, BorderLayout.EAST);
+		listBoxPesos.add(buttons, BorderLayout.EAST);
 
 		frame.add(topPanel, BorderLayout.NORTH);
 		frame.add(midPanel, BorderLayout.CENTER);
@@ -117,6 +151,25 @@ public class GUI {
 
 	}
 
+	private JPanel createListBox(ArrayList<String> listRules) {
+
+		JPanel panel = new JPanel();
+		JList list;
+		DefaultListModel<String> model = new DefaultListModel<>();
+		list = new JList<>(model);
+		JScrollPane pane = new JScrollPane(list);
+		pane.setPreferredSize(new Dimension(100, 170));
+		for (int i = 0; i != listRules.size(); i++) {
+			System.out.println(listRules.get(i));
+			model.addElement(listRules.get(i));
+		}
+		panel.add(pane);
+
+		return panel;
+
+	}
+
+	// devolve o caminho do ficheiro que for selecionado
 	private String getPath(JButton search1, JTextField titleText) {
 		JFileChooser fc = new JFileChooser();
 		fc.setCurrentDirectory(new java.io.File("C:/Users"));
@@ -125,18 +178,46 @@ public class GUI {
 		if (fc.showOpenDialog(search1) == JFileChooser.APPROVE_OPTION) {
 			titleText.setText(fc.getSelectedFile().getAbsolutePath());
 		}
-		return fc.getSelectedFile().getAbsolutePath();
+		if (fc.getSelectedFile().getAbsolutePath() != null) {
+			return fc.getSelectedFile().getAbsolutePath();
+		} else {
+			return null;
+		}
 	}
 
 	// este método cria um painel dando um botão e um textfield
 	private JPanel getPanel(JButton search, JTextField titleText) {
 		JPanel subpanel = new JPanel();
 		subpanel.setLayout(new BorderLayout());
-		titleText.setPreferredSize(new Dimension(200, 24));
+		titleText.setPreferredSize(new Dimension(210, 40));
 		titleText.setEditable(false);
 		subpanel.add(titleText, BorderLayout.WEST);
 		subpanel.add(search, BorderLayout.EAST);
 		return subpanel;
+	}
+
+	public JPanel setNumberOfFakes(int fpResult, int fnResult) {
+		String fpString = Integer.toString(fpResult);
+		String fnString = Integer.toString(fnResult);
+		JPanel pane = new JPanel();
+		JPanel subpanelFN = new JPanel();
+		JPanel subpanelFP = new JPanel();
+		pane.setLayout(new BorderLayout());
+		subpanelFP.setLayout(new BorderLayout());
+		subpanelFN.setLayout(new BorderLayout());
+		JLabel fp = new JLabel("FP:");
+		JLabel fn = new JLabel("FN:");
+		JTextField fpText = new JTextField(fpString);
+		JTextField fnText = new JTextField(fnString);
+		subpanelFN.add(fn, BorderLayout.WEST);
+		subpanelFN.add(fnText, BorderLayout.EAST);
+		subpanelFP.add(fp, BorderLayout.WEST);
+		subpanelFP.add(fpText, BorderLayout.EAST);
+		pane.add(subpanelFN, BorderLayout.WEST);
+		subpanelFN.add(subpanelFP, BorderLayout.NORTH);
+
+		return pane;
+
 	}
 
 	public static void main(String[] args) {
